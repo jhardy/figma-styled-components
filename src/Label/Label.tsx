661144b96b1062e8fd2813c6b11dcc3e6797f6ab
Jared <jared.hardy@looker.com>
2019-08-05 09:39:42 -0700

    Jh/refactor structure (#1)
    Refactor project structure, create folder for each component, change `lib` to `dist`

--- src/Label/Label.tsx
+++ src/Label/Label.tsx
@@ -1,5 +1,5 @@
-import styled, { css } from "styled-components";
-import {Text} from './Text'
+import styled from "styled-components";
+import {Text} from '../Text'
 
 export const Label = styled(Text)`
   display: flex;
