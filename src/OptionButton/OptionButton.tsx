b77ab5814382184a8d3ca0b6bbcfefd5747181d3
Jared <jared.hardy@looker.com>
2019-08-10 15:13:45 -0700

    Jh/option button (#6)
    Add OptionButton and IconButton and bump version to 1.0.16

---
+++ src/OptionButton/OptionButton.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+export interface OptionButtonProps {
+  icon: JSX.Element | string
+  on?: boolean
+  disabled?: boolean
+  onClick?: React.ReactEventHandler
+}
+
+export const OptionButtonContent = styled.span<{on: boolean, disabled: boolean}>`
+  align-items: center;
+  border-radius: 2px;
+  background: ${ (props) => props.on ? 'rgba(0, 0, 0, 0.1)' : 'transparent'};
+  border: none;
+  display:flex;
+  height: 24px;
+  justify-content: center;
+  opacity: ${ (props) => props.disabled ? '0.3' : '1' };
+  outline: none;
+  pointer-events: none;
+  text-align: center;
+  min-width: 24px;
+  width: 24px;
+
+  svg {
+    width: 16px;
+    height: 16px;
+
+  }
+`
+
+const OptionButtonWrapper = styled.button`
+  align-items: center;
+  background: transparent;
+  border: none;
+  border-radius: 2px;
+  display: flex;
+  height: 32px;
+  justify-content: center;
+  width: 32px;
+  outline: none;
+  color: #000;
+
+  &:hover {
+    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1)
+  }
+
+  &:active ${OptionButtonContent} {
+    background: ${(props) => props.disabled ? 'transparent' : 'rgba(0, 0, 0, 0.1)'};
+  }
+
+  &:focus {
+    box-shadow: inset 0 0 0 2px #18A0FB;
+  }
+`
+
+const OptionButtonFactory: React.FC<OptionButtonProps> = ({ icon, on, onClick, disabled, ...props }) => {
+  return(
+    <OptionButtonWrapper onClick={onClick} disabled={disabled || false} {...props}>
+      <OptionButtonContent on={on || false} disabled={disabled || false} >
+        {icon}
+      </OptionButtonContent>
+    </OptionButtonWrapper>
+  )
+}
+
+export const OptionButton = styled(OptionButtonFactory)``
--- stories/index.tsx
+++ stories/index.tsx
@@ -1,115 +1,147 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import "./storybook.css";
-
+import { storiesOf } from '@storybook/react'
+import React from 'react'
 import {
   Button,
   Checkbox,
+  IconButton,
   Input,
   Label,
+  OptionButton,
+  OptionStrip,
   SectionTitle,
   Select,
   Switch,
-  Text,
-  OptionStrip
-} from "../src";
+  Text
+} from '../src'
+import './storybook.css'
 
-
-const testIcon = <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H14V1H0V0ZM0 4H8V5H0V4ZM10 8H0V9H10V8Z" fill="black"/>
+const testIcon = (
+  <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+<path fillRule='evenodd' clip-rule='evenodd' d='M11 4.5V6H12V4.5C12 2.567 10.433 1 8.5 1C6.567 1 5 2.567 5 4.5V6H6V4.5C6 3.11929 7.11929 2 8.5 2C9.88071 2 11 3.11929 11 4.5ZM11 10H12V11.5C12 13.433 10.433 15 8.5 15C6.567 15 5 13.433 5 11.5V10H6V11.5C6 12.8807 7.11929 14 8.5 14C9.88071 14 11 12.8807 11 11.5V10ZM8 5V11H9V5H8Z' fill='currentColor' />
 </svg>
 
-storiesOf("Button", module)
-.add("Variants", () => (
+)
+
+storiesOf('Button', module).add('Variants', () => (
   <div>
-    <Text size="xlarge">Primary</Text>
+    <Text size='xlarge'>Primary</Text>
     <br />
-    <Text size="large">Normal</Text>
+    <Text size='large'>Normal</Text>
     <div>
-      <Button variant="primary">Primary</Button>
-      <Button variant="primary" disabled>
+      <Button variant='primary'>Primary</Button>
+      <Button variant='primary' disabled={true}>
         Primary
       </Button>
     </div>
-    <Text size="large">Destructive</Text>
+    <Text size='large'>Destructive</Text>
     <div>
-      <Button variant="primary" destructive>
+      <Button variant='primary' destructive={true}>
         Primary
       </Button>
-      <Button variant="primary" destructive disabled>
+      <Button variant='primary' destructive={true} disabled={true}>
         Primary
       </Button>
     </div>
 
-    <Text size="xlarge">Secondary</Text>
+    <Text size='xlarge'>Secondary</Text>
     <br />
-    <Text size="large">Normal</Text>
+    <Text size='large'>Normal</Text>
     <div>
       <Button>Primary</Button>
-      <Button disabled>Primary</Button>
+      <Button disabled={true}>Primary</Button>
     </div>
-    <Text size="large">Destructive</Text>
+    <Text size='large'>Destructive</Text>
     <div>
-      <Button destructive>Primary</Button>
-      <Button destructive disabled>
+      <Button destructive={true}>Primary</Button>
+      <Button destructive={true} disabled={true}>
         Primary
       </Button>
     </div>
 
     <div />
   </div>
-));
+))
 
-storiesOf("Checkbox", module)
-.add("Checkbox", () => (
-  <Checkbox label="I am a checkbox, click me" />
-));
-
-storiesOf("Input", module).add("Input", () => (
-  <Input placeholder="I am an input" />
-));
-
-storiesOf("Options Strip", module).add("Options Strip", () => (
+storiesOf('Checkbox', module).add('Checkbox', () => (
+  <Checkbox label='I am a checkbox, click me' />
+))
+ // tslint:disable:jsx-no-lambda
+storiesOf('Icon Button', module).add('Icon Button', () => (
   <div>
-    <Label> Test Label</Label>
-    <OptionStrip options={[{value: '1', label: testIcon}, {value: '2', label: 'Two'}, {value: '3', label:'Three'}]} />
+    <SectionTitle>An IconButton, click me</SectionTitle>
+    <IconButton icon={testIcon} onClick={() => { alert('hello') }} />
+
+    <SectionTitle>An on IconButton</SectionTitle>
+    <IconButton icon={testIcon} on={true} />
+
+    <SectionTitle>An disabled IconButton</SectionTitle>
+    <IconButton icon={testIcon} disabled={true} />
   </div>
 ))
 
-storiesOf("Text Components", module)
-  .add("Section Tile", () => <SectionTitle>I am a section title</SectionTitle>)
-  .add("Label", () => <Label>I am a Label</Label>)
-  .add("Text", () => (
-    <div className="text-components">
-      <div>
-        <Text size="xlarge">Positive Application</Text>
-        <div className="text-block">
+storiesOf('Input', module).add('Input', () => (
+  <Input placeholder='I am an input' />
+))
 
+
+
+storiesOf('Option Button', module).add('Option Button', () => (
+  <div>
+    <SectionTitle>An Option Button, click me</SectionTitle>
+    <OptionButton icon={testIcon} onClick={() => { alert('hello') }} />
+
+    <SectionTitle>An on Option button</SectionTitle>
+    <OptionButton on={true} icon={testIcon} />
+
+    <SectionTitle>A disabled Option button</SectionTitle>
+    <OptionButton disabled={true} icon={testIcon} />
+  </div>
+))
+
+storiesOf('Options Strip', module).add('Options Strip', () => (
+  <div>
+    <Label> Test Label</Label>
+    <OptionStrip
+      options={[
+        { value: '1', label: testIcon },
+        { value: '2', label: 'Two' },
+        { value: '3', label: 'Three' }
+      ]}
+    />
+  </div>
+))
+
+storiesOf('Text Components', module)
+  .add('Section Tile', () => <SectionTitle>I am a section title</SectionTitle>)
+  .add('Label', () => <Label>I am a Label</Label>)
+  .add('Text', () => (
+    <div className='text-components'>
+      <div>
+        <Text size='xlarge'>Positive Application</Text>
+        <div className='text-block'>
           <ul>
             <li>
               <Text>UI11 Normal</Text>
             </li>
             <li>
-              <Text weight="medium">UI11 Medium</Text>
+              <Text weight='medium'>UI11 Medium</Text>
             </li>
             <li>
-              <Text weight="bold">UI11 Bold</Text>
+              <Text weight='bold'>UI11 Bold</Text>
             </li>
           </ul>
 
-
-
           <ul>
             <li>
-              <Text size="medium">UI12 Normal</Text>
+              <Text size='medium'>UI12 Normal</Text>
             </li>
             <li>
-              <Text size="medium" weight="medium">
+              <Text size='medium' weight='medium'>
                 UI12 Medium
               </Text>
             </li>
             <li>
-              <Text size="medium" weight="bold">
+              <Text size='medium' weight='bold'>
                 UI12 Bold
               </Text>
             </li>
@@ -117,96 +149,97 @@ storiesOf("Text Components", module)
         </div>
       </div>
 
-      <Text size="xlarge">Negative Application</Text>
-      <div className="text-block negative-text">
+      <Text size='xlarge'>Negative Application</Text>
+      <div className='text-block negative-text'>
+        <ul>
+          <li>
+            <Text onNegative={true}>UI11 Normal</Text>
+          </li>
+          <li>
+            <Text onNegative={true} weight='medium'>
+              UI11 Medium
+            </Text>
+          </li>
+          <li>
+            <Text onNegative={true} weight='bold'>
+              UI11 Bold
+            </Text>
+          </li>
+        </ul>
 
-
-          <ul>
-            <li>
-              <Text onNegative>UI11 Normal</Text>
-            </li>
-            <li>
-              <Text onNegative weight="medium">
-                UI11 Medium
-              </Text>
-            </li>
-            <li>
-              <Text onNegative weight="bold">
-                UI11 Bold
-              </Text>
-            </li>
-          </ul>
-
-
-
-          <ul>
-            <li><Text onNegative size="medium">
-            UI12 Normal
-          </Text></li>
-            <li> <Text onNegative size="medium" weight="medium">
-            UI12 Medium
-          </Text></li>
-            <li> <Text onNegative size="medium" weight="bold">
-            UI12 Bold
-          </Text></li>
-          </ul>
-
-
-
-
+        <ul>
+          <li>
+            <Text onNegative={true} size='medium'>
+              UI12 Normal
+            </Text>
+          </li>
+          <li>
+            {' '}
+            <Text onNegative={true} size='medium' weight='medium'>
+              UI12 Medium
+            </Text>
+          </li>
+          <li>
+            {' '}
+            <Text onNegative={true} size='medium' weight='bold'>
+              UI12 Bold
+            </Text>
+          </li>
+        </ul>
       </div>
     </div>
-  ));
+  ))
 
-storiesOf("Select", module)
-  .add("Select", () => (
+storiesOf('Select', module)
+  .add('Select', () => (
     <Select
       options={[
-        { label: "Option One", value: "one" },
-        { label: "Option Two", value: "two" },
-        { label: "Option Three", value: "three" }
+        { label: 'Option One', value: 'one' },
+        { label: 'Option Two', value: 'two' },
+        { label: 'Option Three', value: 'three' }
       ]}
     />
   ))
-  .add("Select with a placeholder", () => (
+  .add('Select with a placeholder', () => (
     <Select
-      placeholder="Select an option"
+      placeholder='Select an option'
       options={[
-        { label: "Option One", value: "one" },
-        { label: "Option Two", value: "two" },
-        { label: "Option Three", value: "three" }
+        { label: 'Option One', value: 'one' },
+        { label: 'Option Two', value: 'two' },
+        { label: 'Option Three', value: 'three' }
       ]}
     />
   ))
-  .add("Select with Groups", () => (
+  .add('Select with Groups', () => (
+    // tslint:disable:object-literal-sort-keys
     <Select
-      placeholder="Select an option"
+      placeholder='Select an option'
       options={[
         {
-          label: "Group 1",
+          label: 'Group 1',
           group: [
-            { label: "Item 1", value: "1" },
-            { label: "Item 2", value: "2" }
+            { label: 'Item 1', value: '1' },
+            { label: 'Item 2', value: '2' }
           ]
         },
         {
-          label: "Group 2",
+          label: 'Group 2',
           group: [
-            { label: "Item 3", value: "3" },
-            { label: "Item 4", value: "4" }
+            { label: 'Item 3', value: '3' },
+            { label: 'Item 4', value: '4' }
           ]
         },
         {
-          label: "Group 3",
+          label: 'Group 3',
           group: [
-            { label: "Item 5", value: "5" },
-            { label: "Item 6", value: "6" }
+            { label: 'Item 5', value: '5' },
+            { label: 'Item 6', value: '6' }
           ]
         }
       ]}
     />
-  ));
+  ))
 
-storiesOf("Switch", module).add("A Switch", () => (
-  <Switch label="I am a switch, click me" />
-));
+storiesOf('Switch', module).add('A Switch', () => (
+  <Switch label='I am a switch, click me' />
+))
